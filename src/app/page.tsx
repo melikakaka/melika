69c9bb68ab46b1d2b894e05666151216c42f0f6a
melikakaka <melikamoshref2003@gmail.com>
2024-01-10 00:57:02 +0330

    add final changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import { useState } from 'react'
 
 export default function Home() {
-  const [isshow,setisshow]=useState<boolean>(true)
+  const [isshow,setisshow]=useState<boolean>(false)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 text-white text-3xl">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24 text-white text-5xl">
      <p>
-      ملیکا
+سایت ملیکا مشرف :))))
      </p>
-     <button onClick={()=>{setisshow(!isshow)}} >
-      دکمه نشون دادن ملیکا
+     <button onClick={()=>{setisshow(!isshow)}} className='shadow-2xl bg-pink-950 p-8 rounded-md mt-8 mb-8' >
+      {isshow?"دکمه قایم شدن ملیکا :(":"دکمه پیدا شدن ملیکا :)"}   
      </button>
      {isshow?<Image src={'/1.jpg'} alt='' width={400} height={600}/>:null}
        
